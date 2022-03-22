e4c00b97ff8f382119e484ff83639f9980b54705
Jacob Yarborough <jayarborough95@gmail.com>
2022-03-22 16:40:11 -0600

    Create middleware

--- first_rack_app.rb
+++ first_rack_app.rb
@@ -1,6 +1,6 @@
 require 'rack'
 
-
+# creating first rack app 
 handler = Rack::Handler::Thin
 
 class RackApp 
@@ -10,5 +10,24 @@ class RackApp
     [200, {"Content-Type" => "text/plain"}, ["Hello from Rack"]]
   end 
 end 
+
+#creating first middleware
+class FilterLocalHost
+  def initialize(app)
+    @app = app 
+  end 
+
+  def call(env)
+    req = Rack::Request.new(env)
+
+    if req.ip == "::1"
+      [403, {}, "Localhost not allowed"]
+    else 
+      @app.call(env)
+    end 
+  end 
+end 
+
+
 
 handler.run RackApp.new
